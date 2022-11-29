a5ed3cabdcc196a500702de3f37d66ee5e3eeb89
priyansh4320 <69320370+priyansh4320@users.noreply.github.com>
2022-11-29 14:20:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import Profile from "./PortfolioContainer/Home/Profile";
+import "./App.css"
+import Footer from './PortfolioContainer/Home/Footer'
+
+function App() {
+  return (
+    <div className="App">
+     <Profile/>
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
